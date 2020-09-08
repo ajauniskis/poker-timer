f136a65a3c93aa3d990ea32ec3ddc44feeead267
ajauniskis <56760860+ajauniskis@users.noreply.github.com>
2020-09-08 22:21:54 +0300

    Delete sound.py
    deleting not needed .py

--- sound.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from tkinter import *
-root = Tk()
-pygame.init()
-def play():
-    pygame.mixer.music.load("example.mp3") #Loading File Into Mixer
-    pygame.mixer.music.play() #Playing It In The Whole Device
-Button(root,text="Play",command=play).pack()
-root.mainloop()
